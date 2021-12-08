92af026134d157f744ddd4330e5f2baeef711e3c
youngjoon kim <bigdream9609@gmail.com>
2021-12-08 16:10:50 +0900

    refactor: Apis.java
    Constructor Injection Annotations
    Generic type specification
    Duplicate code removal

--- src/main/java/com/fastcampus/investment/api/Apis.java
+++ src/main/java/com/fastcampus/investment/api/Apis.java
@@ -6,48 +6,48 @@ import com.fastcampus.investment.dto.ProductResponse;
 import com.fastcampus.investment.service.InvestmentService;
 import com.fastcampus.investment.service.ProductService;
 import com.fastcampus.investment.util.JsonUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
+@RequiredArgsConstructor
 public class Apis {
     private static final String USER_ID = "X-USER-ID";
 
     private final ProductService productService;
     private final InvestmentService investmentService;
 
-    @Autowired
-    public Apis(ProductService productService, InvestmentService investmentService) {
-        this.productService = productService;
-        this.investmentService = investmentService;
-    }
-
     @GetMapping("product")
-    public ResponseEntity<?> inquireInvestableProducts() {
+    public ResponseEntity<Map<String, List<ProductResponse>>> inquireInvestableProducts() {
         List<ProductResponse> products = productService.inquireInvestableProducts();
         return new ResponseEntity<>(JsonUtil.convert(products), HttpStatus.OK);
     }
 
     @GetMapping("investment")
-    public ResponseEntity<?> invest(@RequestHeader(value="X-USER-ID") Long userId) {
+    public ResponseEntity<Map<String, List<InvestmentResponse>>> searchInvestment(@RequestHeader(value = USER_ID) Long userId) {
         List<InvestmentResponse> investmentResponses = investmentService.searchInvestment(userId);
         return new ResponseEntity<>(JsonUtil.convert(investmentResponses), HttpStatus.OK);
     }
 
     @PostMapping("investment")
-    public ResponseEntity<?> searchInvestment(@RequestHeader(value="X-USER-ID") Long userId, @RequestParam Long productId, @RequestParam Long investAmount) {
+    public ResponseEntity<Map<String, InvestmentResponse>> invest(@RequestHeader(value = USER_ID) Long userId,
+                                                                  @RequestParam Long productId,
+                                                                  @RequestParam Long investAmount) {
         InvestmentResponse investmentResponse = investmentService.invest(userId, productId, investAmount);
         return new ResponseEntity<>(JsonUtil.convert(investmentResponse), HttpStatus.OK);
     }
 
     @PutMapping("investment/{productId}")
-    public ResponseEntity<?> updateInvestment(@RequestHeader(value="X-USER-ID") Long userId, @PathVariable("productId") Long productId, @RequestParam InvestmentStatus status) {
-        List<InvestmentResponse> investmentResponses = investmentService.updateInvestmentStatus(userId, productId, status);
+    public ResponseEntity<Map<String, List<InvestmentResponse>>> updateInvestment(@RequestHeader(value = USER_ID) Long userId,
+                                                                                  @PathVariable("productId") Long productId,
+                                                                                  @RequestParam InvestmentStatus status) {
+        List<InvestmentResponse> investmentResponses = investmentService.updateInvestment(userId, productId, status);
         return new ResponseEntity<>(JsonUtil.convert(investmentResponses), HttpStatus.OK);
     }
 }
