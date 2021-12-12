b9de985cec4f9b6f129c6acdd5746c3fcdff76ea
youngjoon kim <bigdream9609@gmail.com>
2021-12-12 16:41:13 +0900

    refactor: InvestmentRepository.java
    Add optional

--- src/main/java/com/fastcampus/investment/repository/InvestmentRepository.java
+++ src/main/java/com/fastcampus/investment/repository/InvestmentRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
-    List<Investment> findByProductId(Long id);
-    List<Investment> findByProduct(Product product);
-    List<Investment> findByUserId(Long userId);
+    Optional<List<Investment>> findByProduct(Product product);
+    Optional<List<Investment>> findByUserId(Long userId);
+    Optional<Integer> countByProduct(Product product);
 }
