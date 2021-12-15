226557b2b29c3bf7962d3ab2dfc7aa9bde4091bc
youngjoon kim <bigdream9609@gmail.com>
2021-12-15 14:10:51 +0900

    refactor: Add API Request validation

--- src/main/java/com/fastcampus/investment/api/Apis.java
+++ src/main/java/com/fastcampus/investment/api/Apis.java
@@ -8,6 +8,7 @@ import com.fastcampus.investment.service.InvestmentService;
 import com.fastcampus.investment.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.*;
@@ -17,6 +18,7 @@ import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("api")
+@Validated
 @RequiredArgsConstructor
 public class Apis {
     private static final String USER_ID = "X-USER-ID";
@@ -32,24 +34,24 @@ public class Apis {
     }
 
     @GetMapping("investment")
-    public ResponseEntity<Message<List<InvestmentResponse>>> searchInvestment(@RequestHeader(value = USER_ID) @NotNull Long userId) {
+    public ResponseEntity<Message<List<InvestmentResponse>>> searchInvestment(@RequestHeader(value = USER_ID) @NotNull @Positive Long userId) {
         List<InvestmentResponse> investmentResponses = investmentService.searchInvestment(userId);
         Message<List<InvestmentResponse>> message = Message.OK(investmentResponses);
         return new ResponseEntity<>(message, OK);
     }
 
     @PostMapping("investment")
-    public ResponseEntity<Message<InvestmentResponse>> invest(@RequestHeader(value = USER_ID) @NotNull Long userId,
-                                                              @RequestParam @NotNull Long productId,
-                                                              @RequestParam @NotNull Long investAmount) {
+    public ResponseEntity<Message<InvestmentResponse>> invest(@RequestHeader(value = USER_ID) @NotNull @Positive Long userId,
+                                                              @RequestParam @NotNull @Positive Long productId,
+                                                              @RequestParam @PositiveOrZero Long investAmount) {
         InvestmentResponse investmentResponse = investmentService.invest(userId, productId, investAmount);
         Message<InvestmentResponse> message = Message.OK(investmentResponse);
         return new ResponseEntity<>(message, OK);
     }
 
     @PutMapping("investment/{productId}")
-    public ResponseEntity<Message<List<InvestmentResponse>>> updateInvestment(@RequestHeader(value = USER_ID) @NotNull Long userId,
-                                                                              @PathVariable("productId") @NotNull Long productId,
+    public ResponseEntity<Message<List<InvestmentResponse>>> updateInvestment(@RequestHeader(value = USER_ID) @NotNull @Positive Long userId,
+                                                                              @PathVariable("productId") @NotNull @Positive Long productId,
                                                                               @RequestParam @NotBlank InvestmentStatus status) {
         List<InvestmentResponse> investmentResponses = investmentService.updateInvestment(userId, productId, status);
         Message<List<InvestmentResponse>> message = Message.OK(investmentResponses);
