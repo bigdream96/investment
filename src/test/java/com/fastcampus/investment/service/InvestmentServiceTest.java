29878daad5f176b1a1e781ced650060843d84da7
youngjoon kim <bigdream9609@gmail.com>
2021-12-24 17:24:19 +0900

    feat: add service test case
    Added case of failure

--- src/test/java/com/fastcampus/investment/service/InvestmentServiceTest.java
+++ src/test/java/com/fastcampus/investment/service/InvestmentServiceTest.java
@@ -3,6 +3,7 @@ package com.fastcampus.investment.service;
 import com.fastcampus.investment.domain.Investment;
 import com.fastcampus.investment.domain.Product;
 import com.fastcampus.investment.dto.response.InvestmentResponse;
+import com.fastcampus.investment.exception.APIException;
 import com.fastcampus.investment.repository.InvestmentRepository;
 import com.fastcampus.investment.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,10 +18,9 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import static com.fastcampus.investment.constants.InvestmentStatus.CANCELED;
-import static com.fastcampus.investment.constants.InvestmentStatus.INVESTED;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.fastcampus.investment.constants.ErrorCode.*;
+import static com.fastcampus.investment.constants.InvestmentStatus.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -43,7 +43,7 @@ class InvestmentServiceTest {
         product = Product.builder()
                 .id(1L)
                 .title("TOBE-RICH of Warren Buffett")
-                .totalInvestingAmount(100000000L)
+                .totalInvestingAmount(100_000_000L)
                 .startedAt(LocalDate.of(2021, 1, 1).minusDays(3))
                 .finishedAt(LocalDate.of(2021, 12, 31).plusDays(3))
                 .build();
@@ -66,6 +66,33 @@ class InvestmentServiceTest {
                 () -> assertEquals(INVESTED, response.getStatus())
         );
 
+    }
+
+    @Test
+    @DisplayName("잘못된 투자 CASE1. 찾을 수 없는 투자상품")
+    void failInvestmentByNotFoundProduct() {
+        Long userId = 1L;
+        Long investAmount = 10000L;
+
+        when(productRepository.findById(product.getId())).thenThrow(new APIException(NO_PRODUCT_DATA));
+
+        assertThrows(APIException.class, () -> investmentService.invest(userId, product.getId(), investAmount));
+    }
+
+    @Test
+    @DisplayName("잘못된 투자 CASE2. 투자할 수 없는 금액")
+    void failInvestmentByNotValidData() {
+        Long userId = 1L;
+        Long investAmount = 1_000_000_000L;
+
+        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+        when(investmentRepository.save(any(Investment.class))).then(returnsFirstArg());
+
+        InvestmentResponse response = investmentService.invest(userId, product.getId(), investAmount);
+
+        assertAll(
+                () -> assertEquals(FAIL, response.getStatus())
+        );
     }
 
     @Test
@@ -114,5 +141,32 @@ class InvestmentServiceTest {
                 () -> assertEquals(product.getId(), response.getProductResponse().getId()),
                 () -> assertEquals(CANCELED, response.getStatus())
         );
+    }
+
+    @Test
+    @DisplayName("투자취소 실패 CASE1. 찾을 수 없는 투자내역")
+    void failCancelInvestmentByNotFoundInvestment() {
+        Long userId = 1L;
+        Long investmentId = 999L;
+
+        when(investmentRepository.findById(investmentId)).thenThrow(new APIException(NO_INVESTMENT_DATA));
+
+        assertThrows(APIException.class, () -> investmentService.updateInvestment(userId, investmentId, CANCELED));
+    }
+
+    @Test
+    @DisplayName("투자취소 실패 CASE2. 일치하지 않는 유저ID")
+    void failCancelInvestmentByNotMatchUserId() {
+        Long investmentId = 999L;
+        Investment investment = Investment.builder()
+                .id(investmentId)
+                .product(product)
+                .userId(1L)
+                .status(CANCELED)
+                .build();
+
+        when(investmentRepository.findById(investmentId)).thenReturn(Optional.of(investment));
+
+        assertThrows(APIException.class, () -> investmentService.updateInvestment(2L, investmentId, CANCELED));
     }
 }
