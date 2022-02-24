7f57b2ba04d70266699fa810970febf789b3b9a8
bigdream96 <bigdream9609@gmail.com>
2022-02-24 21:54:56 +0900

    refactor: Message.java

--- src/main/java/com/fastcampus/investment/dto/Message.java
+++ src/main/java/com/fastcampus/investment/dto/Message.java
@@ -1,27 +1,17 @@
 package com.fastcampus.investment.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
+@Getter
+@ToString
 public class Message<T> {
-    // api성공여부
     private Boolean isSuccess;
-
-    // api부가설명
     private String description;
-
-    // 데이터
     @JsonProperty("data")
     private T data;
 
-    // DATA OK
     public static <T> Message<T> ok(T data) {
         return Message.<T>builder()
                 .isSuccess(true)
@@ -30,7 +20,6 @@ public class Message<T> {
                 .build();
     }
 
-    // ERROR
     public static <T> Message<T> error(String description) {
         return Message.<T>builder()
                 .isSuccess(false)
